ec0fc2bf39305eaea32419c73caa67fe63f6a3e3
José Gomes de Menezes <josegomesd6@gmail.com>
2023-08-20 10:56:36 -0300

    Update

--- python/exercicio/listTwo/descending.py
+++ python/exercicio/listTwo/descending.py
@@ -4,6 +4,7 @@ numeros = [int(input("Digite o primeiro número: ")),
             int(input("Digite o segundo número: ")),
             int(input("Digite o terceiro número: "))]
 
+# Ordenar os números em ordem decrescente
 numeros.sort(reverse = True)
 
 print("Números em ordem decrescente:", numeros[0], numeros[1], numeros[2])
