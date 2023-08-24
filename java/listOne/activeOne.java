64bd0470de29ac0b7137ac57de7ee5e1dc23109b
José Gomes de Menezes <josegomesd6@gmail.com>
2023-08-24 14:41:00 -0300

    First Commit Java

---
+++ java/listOne/activeOne.java
@@ -0,0 +1,21 @@
+package listOne;
+import java.util.Scanner;
+
+public class activeOne {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in, "UTF-8");
+
+    System.out.println("Digite seu nome: ");
+    String name = scanner.nextLine();
+
+    System.out.println("Digite a sua idade: ");
+    Integer age = Integer.parseInt(scanner.nextLine());
+    
+    System.out.println("Digite onde você nasceu: ");
+    String city = scanner.nextLine();
+
+    System.out.println("Oi " + name + " você tem " + age + " e nasceu no " + city);
+
+    scanner.close();
+  }
+}
--- python/exercicio/listOne/areaquadrado.py
+++ python/exercicio/listOne/areaquadrado.py
@@ -3,3 +3,5 @@ lado = float(input("Digite o valor dos lados desse quadrado: "))
 area = (lado * lado) * 2
 
 print(f"O dobro da area desse quadrado é  {area:.0f}")
+
+print("Olá Mundo")
